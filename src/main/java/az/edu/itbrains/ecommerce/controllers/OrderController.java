56d3e25b467233c0263fa5d8688d97856d8a2c9d
sananabbasov <sanan_abbasov@list.ru>
2025-03-06 10:54:15 +0400

    post

--- src/main/java/az/edu/itbrains/ecommerce/controllers/OrderController.java
+++ src/main/java/az/edu/itbrains/ecommerce/controllers/OrderController.java
@@ -1,10 +1,11 @@
 package az.edu.itbrains.ecommerce.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 public class OrderController {
 
-    @GetMapping("/order")
+    @PostMapping("/order")
     public String order(){
         return "index.html";
     }
