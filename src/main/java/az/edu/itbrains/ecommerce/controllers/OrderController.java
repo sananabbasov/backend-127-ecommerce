f5878619eccbed6eddc2655e87a02fb229dcb869
sananabbasov <sanan_abbasov@list.ru>
2025-03-06 10:51:36 +0400

    order method is ready

--- src/main/java/az/edu/itbrains/ecommerce/controllers/OrderController.java
+++ src/main/java/az/edu/itbrains/ecommerce/controllers/OrderController.java
@@ -1,4 +1,11 @@
 package az.edu.itbrains.ecommerce.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class OrderController {
+
+    @GetMapping("/order")
+    public String order(){
+        return "index.html";
+    }
 }
